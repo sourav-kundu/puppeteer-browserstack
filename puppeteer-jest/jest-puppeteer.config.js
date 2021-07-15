7f61a896c58b4e8ab60c454ee931eeabc7640c6f
sourav-kundu <66624530+sourav-kundu@users.noreply.github.com>
2021-07-15 15:02:05 +0530

    fix: test name changed

--- puppeteer-jest/jest-puppeteer.config.js
+++ puppeteer-jest/jest-puppeteer.config.js
@@ -3,7 +3,7 @@ const caps_chrome = {
     'browser_version': 'latest',
     'os': 'osx',
     'os_version': 'big sur',
-    'name': 'Puppeteer-jest test on Chromium',
+    'name': 'Puppeteer-jest test on Chrome',
     'build': 'puppeteer-jest-build-2',
     'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
     'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
@@ -15,7 +15,7 @@ const caps_firefox = {
     'os': 'osx',
     'os_version': 'big sur',
     'name': 'Puppeteer-jest test on Firefox',
-    'build': 'puppeteer-jest-build-1',
+    'build': 'puppeteer-jest-build-2',
     'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
     'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
 };
@@ -25,8 +25,8 @@ const caps_edge = {
     'browser_version': 'latest',
     'os': 'osx',
     'os_version': 'big sur',
-    'name': 'Puppeteer-jest test on Webkit',
-    'build': 'puppeteer-jest-build-1',
+    'name': 'Puppeteer-jest test on Edge',
+    'build': 'puppeteer-jest-build-2',
     'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
     'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY'
 };
